029926191606ef504c7a05a3b9b19c3696cf8436
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2024-08-20 03:54:55 +0530

    Init commit with basic tracker connection

---
+++ go.mod
@@ -0,0 +1,7 @@
+module rainstorm/tracker
+
+go 1.22.5
+
+replace rainstorm/common => ../common
+
+require rainstorm/common v0.0.0-00010101000000-000000000000 // indirect
---
+++ tracker.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"net"
+	"rainstorm/common"
+	"fmt"
+	"encoding/json"
+	"errors"
+);
+
+var fileDict map[string]common.FileDownloadData;
+
+func sendDownloadData(fileID string, conn net.Conn) error {
+	res, ok := fileDict[fileID]
+	if (!ok) {
+		conn.Write([]byte("{\"error\": \"Unknown file ID\"}"))
+		return errors.New("Unknown file ID")
+	}
+	buf, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	n, err := conn.Write(buf)
+	if n == 0 {
+		return errors.New("0 bytes sent")
+	} else  {
+		return err
+	}
+}
+
+func trackerHandler(conn net.Conn) {
+	defer conn.Close();
+	buf := make([]byte, 1024)
+	var n int
+	var err error
+	msg := make(map[string]interface{});
+	otherAddr := conn.RemoteAddr()
+
+	for true {
+		n, err = conn.Read(buf)
+		if (err != nil) {
+			fmt.Println("Error while reading from addr ", otherAddr, ": ", err);
+			return;
+		}
+		if (n == 0) {
+			fmt.Println("Error from addr ", otherAddr, ": 0 bytes read");
+			return
+		}
+
+		fmt.Println("Received from ", otherAddr, ": ", string(buf));
+		err = json.Unmarshal(buf[:n], &msg)
+		if (err != nil) {
+			fmt.Println("Error from addr ", otherAddr, ": ", err);
+			return
+		}
+		fmt.Println("Dict: ");
+		for k, v := range msg {
+			fmt.Printf("%v: %v\n", k, v);
+		}
+		if (msg["class"] != "init") {
+			fmt.Println("Expected msg of class init, got ", msg["class"]);
+			return;
+		}
+
+		switch (msg["type"]) {
+		case "download_start":
+			// send download data
+			file_id, ok := msg["file_id"].(string)
+			if (!ok) {
+				fmt.Println("Missing file_id")
+				return
+			}
+			err = sendDownloadData(file_id, conn)
+		default:
+			fmt.Println("Unexpected msg type ", msg["type"]);
+			return
+		}
+	}
+}
+
+func main() {
+	// code to accept new files
+	fileDict = make(map[string]common.FileDownloadData);
+	fileDict["testfileid"] = common.FileDownloadData{
+		FileID: "testfileid", 
+		FileName:"testfilename", 
+		Peers:[]common.Peer{
+			{IP: "127.0.0.1", Port: 8081},
+		}};
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%v", common.SERVER_PORT));
+	//defer listener.Close()
+	if (err != nil) {
+		fmt.Printf("Error while listening on port %d: %s", common.SERVER_PORT, err);
+		return;
+	}
+
+	for (true) {
+		conn, err := listener.Accept()
+		if (err != nil) {
+			fmt.Println("Error accepting connection ", conn, ": ", err); 
+			return;
+		}
+		fmt.Printf("Accepted conn from %v", conn.RemoteAddr().String());
+		go trackerHandler(conn)
+	}
+}
